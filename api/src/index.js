a670fecdfcce2f027e7baa646485f22e12256297
jorik <rooderuler@gmail.com>
2021-08-29 23:00:55 +0300

    Complete: 019 Сети Docker

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { connectDb } = require("./helpers/db");
-const { port, host, db } = require("./configuration");
+const {connectDb} = require("./helpers/db");
+const {port, host, db, authApiUrl} = require("./configuration");
+const axios = require("axios");
+const {response} = require("express");
 
 const app = express();
 const postSchema = new mongoose.Schema({
@@ -15,8 +17,8 @@ const startServer = () => {
     console.log(`Our host is ${host}`);
     console.log(`Database url ${db}`);
 
-    const silence = new Post({ name: "Silence" });
-    silence.save(function(err, savedSilence) {
+    const silence = new Post({name: "Silence"});
+    silence.save(function (err, savedSilence) {
       if (err) return console.error(err);
       console.log("savedSilence vmeste s volumes", savedSilence);
     });
@@ -27,7 +29,23 @@ app.get("/test", (req, res) => {
   res.send("Our api server is working correctly");
 });
 
+app.get('/api/testapidata', (req, res) => {
+  res.json({
+    testwithapi: true
+  })
+})
+
+app.get("/testwithcurrentuser", (req, res) => {
+  // console.log("authApiUrl", authApiUrl);
+  axios.get(authApiUrl + "/currentUser").then(response => {
+    res.json({
+      testwithcurrentuser: true,
+      currentUserFromAuth: response.data
+    })
+  })
+});
+
 connectDb()
-    .on("error", console.log)
-    .on("disconnected", connectDb)
-    .once("open", startServer);
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
--- auth/src/index.js
+++ auth/src/index.js
@@ -1,22 +1,38 @@
 const express = require("express");
-const { port, host, db } = require("./configuration");
-const { connectDb } = require("./helpers/db");
-
+const {port, host, db, apiUrl} = require("./configuration");
+const {connectDb} = require("./helpers/db");
+const axios = require("axios");
+const {response} = require("express");
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.send("Our authentication server is working correctly");
-});
-
 const startServer = () => {
-  app.listen(port, () => {
-    console.log(`Started authentication service on port ${port}`);
-    console.log(`Our host is ${host}`);
-    console.log(`Database url ${db}`);
-  });
+    app.listen(port, () => {
+        console.log(`Started authentication service on port ${port}`);
+        console.log(`Our host is ${host}`);
+        console.log(`Database url ${db}`);
+    });
 };
 
+app.get("/test", (req, res) => {
+    res.send("Сервер аутентификации работает корректно");
+});
+
+app.get('/testwithapidata', (req, res) => {
+    axios.get(apiUrl + '/testapidata').then(response => {
+        res.json({
+            testapidata: response.data.testwithapi
+        })
+    })
+})
+
+app.get("/api/currentUser", (req, res) => {
+    res.json({
+        id: "1234",
+        email: "foo@gmail.com"
+    });
+});
+
 connectDb()
-  .on("error", console.log)
-  .on("disconnected", connectDb)
-  .once("open", startServer);
+    .on("error", console.log)
+    .on("disconnected", connectDb)
+    .once("open", startServer);
