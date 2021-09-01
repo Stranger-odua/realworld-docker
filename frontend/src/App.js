f18bcd9c55716ec07e3aa53a6f642476e2d8c49d
jorik <rooderuler@gmail.com>
2021-09-01 19:54:42 +0300

    Complete: 021 Последние улучшения
    Веб сокеты так и не заработали, поэтому автообновление при изменениях не работает ((

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!!! DEV сервак
+          Edit <code>src/App.js</code> and save to reload!!!! DEV сервак1
         </p>
         <a
           className="App-link"
