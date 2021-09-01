8239f2a665055f542c754805c69d8c356c99e3b6
jorik <rooderuler@gmail.com>
2021-09-01 20:12:21 +0300

    Complete: 012 Хранилища 013 Сервис авторизации 014 Сервис фронтенда 015 Запускаем фронтенд в продакшен моде 016 Docker exec 017 Настройка Nginx 018 Проксируем API запросы 019 Сети Docker 020 Проксирование фронтенда 021 Последние улучшения
    Веб сокеты заработали, автообновление при изменениях работает!
    Для этого еще раз скопировал с мануала
    https://www.nginx.com/blog/nginx-nodejs-websockets-socketio/
    с раздела Virtual Host Configuration
    но еще и в той же очыередности и заработало.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!!! DEV сервак1
+          Edit <code>src/App.js</code> and save to reload!!!! DEV сервак
         </p>
         <a
           className="App-link"
